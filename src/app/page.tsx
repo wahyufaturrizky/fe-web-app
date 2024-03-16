c6169eaa2e68f0c27fb395ac4890b70ccd741ad0
wahyufaturrizki <wahyufaturrizkyy@gmail.com>
2024-03-16 14:15:27 +0800

    FEAT: set up chat bot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
 "use client";
 
 import SendbirdApp from "@sendbird/uikit-react/App";
+import { ChatAiWidget } from "@sendbird/chat-ai-widget";
+
 import "@sendbird/uikit-react/dist/index.css";
 
 const APP_ID = process.env.NEXT_PUBLIC_YOUR_APP_ID;
 const USER_ID = process.env.NEXT_PUBLIC_YOUR_USER_ID;
 
 export default function Home() {
+  function isIOS() {
+    const userAgent = window.navigator.userAgent;
+    const platform = window.navigator?.userAgentData?.platform || window.navigator.platform;
+    const iosPlatforms = ["iPhone", "iPad", "iPod"];
+    if (iosPlatforms.indexOf(platform) !== -1) {
+      return true;
+    }
+    return false;
+  }
+  const isMobile = isIOS();
+
   return (
     <div className="App">
       <SendbirdApp
@@ -16,7 +29,13 @@ export default function Home() {
         // Specify the user ID you've created on the dashboard.
         // Or you can create a user by specifying a unique userId.
         userId={USER_ID}
+        breakpoint={isMobile || "567px"}
       />
+
+      {/* <ChatAiWidget
+            applicationId={APP_ID} // Your Sendbird Application ID
+            botId="khan-academy-bot" // Your Bot ID
+        /> */}
     </div>
   );
 }
